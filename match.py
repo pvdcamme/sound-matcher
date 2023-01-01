43fc9bc5907294039764bf338b2226a793bf862f
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2023-01-01 20:45:50 +0100

    Looking back into batching the FFT operations. Does seem to offer quite a speedup

--- match.py
+++ match.py
@@ -76,7 +76,7 @@ def chunked_read(file_name, size, start=0):
 
 def create_hashes(length=2**16, name=FILE_NAME):
     """
-        
+      Splits up the file in multiple hashes of length.
     """
     def calculate_hash(sorted_idx, moment):
         seen = set(range(4))
@@ -93,20 +93,21 @@ def create_hashes(length=2**16, name=FILE_NAME):
     start_moment = time.time()
     stream = cupy.cuda.Stream(non_blocking=True)
 
-    for idx, part in enumerate(chunked_read(name, length), start=0):
-        sample_start = idx * length 
-        if idx % 1024 == 0:
-          idx_speed = idx / (time.time() - start_moment)
-          msg = f"{idx_speed:.2f} part/s -- {len(chunk_hashes)} hashes -- {sample_start/ 44100:.2f}s"
-          print(msg)
 
-        with stream:
+    with stream:
+      for idx, part in enumerate(chunked_read(name, length), start=0):
+          sample_start = idx * length 
+          if idx % 1024 == 0:
+            idx_speed = idx / (time.time() - start_moment)
+            msg = f"{idx_speed:.2f} part/s -- {len(chunk_hashes)} hashes -- {sample_start/ 44100:.2f}s"
+            print(msg)
+
           bb = cupy.fft.rfft(part)
           max_val = cupy.abs(bb)
           sorted_idx = cupy.argsort(max_val)
           chunk_hashes.append(calculate_hash(sorted_idx, sample_start))
+          stream.synchronize()
     return chunk_hashes
-
 
 def show_match(file_name, start_idx, end_idx, length):
     start_data = next(chunked_read(file_name, length, start_idx * length))
@@ -154,9 +155,90 @@ def calc_closest_pair_gpu(chunks):
     return [(float(mm), int(idx_1), int(idx_2)) for mm, idx_1, idx_2 in result]
 
 
-chunks = create_hashes(2**16)
+#chunks = create_hashes(2**12)
+#start, closest_results, end = (time.time(), calc_closest_pair_gpu(chunks),
+#                               time.time())
+#print(f"GPU Closest in {len(chunks) * len(chunks) / (end - start)} assoc/sec")
+#
 
-start, closest_results, end = (time.time(), calc_closest_pair_gpu(chunks),
-                               time.time())
-print(f"GPU Closest in {len(chunks) * len(chunks) / (end - start)} assoc/sec")
+together_count = 512
+vals = cupy.random.random(together_count * 2** 16, dtype=cupy.float32)
+def multiple_together(vals):
+  stream = cupy.cuda.Stream(non_blocking=True)
+  with stream:
+    vals = vals.reshape((together_count, 2**16))
+    fft = cupy.fft.rfft(vals, axis=1)
+    fft = cupy.abs(fft)
+    fft[:,0] = 0
+    max_idx = cupy.argmax(fft, axis=1)
+  stream.synchronize()
+  return max_idx.get()
 
+def multiple_seperate(vals):
+  result = []
+  stream = cupy.cuda.Stream(non_blocking=True)
+  with stream:
+    for idx in range(together_count):
+      row = cupy.fft.rfft(vals[idx * 2**16: (idx+1) * 2**16])
+      row = cupy.abs(row)
+      row[0] = 0
+      max_idx = cupy.argmax(row)
+      result.append(max_idx)
+  return [float(val) for val in result]
+
+
+
+print(f"together: {multiple_together(vals)}")
+print(f"seperate: {multiple_seperate(vals)}")
+
+print("starting benchmark")
+
+res = cupyx.profiler.benchmark(multiple_together, (vals,), n_repeat=200)
+print(f"benchmark together:  {res}")
+
+res = cupyx.profiler.benchmark(multiple_seperate, (vals,), n_repeat=200)
+print(f"benchmark seperate:  {res}")
+
+
+
+  
+
+
+def snr_vs_duration(file_name):
+  duration = 1024
+  durations = [] 
+  avg_snr = []
+  min_snr = []
+  max_snr = []
+
+  stream = cupy.cuda.Stream(non_blocking=True)
+  while duration < 1024 * 1024:
+    start = time.time()
+    snrs = []
+    with stream:
+      interesting = (10e3 / 44100)
+      for idx, part in zip(range(8 * 1024), chunked_read(file_name, duration)):
+        fft = cupy.abs(cupy.fft.rfft(part))
+        fft = fft[0:int(duration * interesting)]
+        largest = cupy.max(fft)
+        avg = cupy.median(fft)
+        snrs.append(largest / avg)
+
+    snrs = np.array([float(val) for val in snrs])
+    durations.append(duration / 44100.)
+    avg_snr.append(np.quantile(snrs,0.5))
+    min_snr.append(np.quantile(snrs,0.2))
+    max_snr.append(np.quantile(snrs, 0.8))
+
+    print(f" {duration} :: {time.time() - start}")
+    duration = (int(1.1 * duration) >> 3) << 3
+
+
+
+  plt.plot(durations, avg_snr)
+  plt.plot(durations, max_snr)
+  plt.plot(durations, min_snr)
+  plt.show()
+
+#snr_vs_duration(FILE_NAME)
+    
